b15bbb8e9c957a953441c67149594157eccad89c
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-16 10:14:24 +0530

    Add files via upload

---
+++ CheckVOWELorCONSONENT.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char n;      // Character should not be capital.
+    printf("Enter Any Character:- ");
+    scanf("%c",&n);
+    if( n =='a' || n =='e' || n =='i' || n =='o' || n =='u')
+    {
+        printf("Vowel!");
+    }
+    else
+    {
+        printf("Consonent!");
+    }
+    return 0;
+}
